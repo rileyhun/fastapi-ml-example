2f1707379d0f73f5301a4c81514276df49159118
Riley Hun <riley.hun@coursehero.com>
2021-10-18 00:01:19 -0700

    rhun - load testing

--- load_test/locust_file.py
+++ load_test/locust_file.py
@@ -1,28 +1,27 @@
 from locust import HttpUser, task
 import pandas as pd
 import random
-import os
+from sklearn import datasets
 
-os.environ['AWS_ACCESS_KEY_ID'] = ""
-os.environ['AWS_SECRET_ACCESS_KEY'] = ""
-os.environ['AWS_SESSION_TOKEN'] = ""
+dataset = datasets.load_wine()
+X = dataset.data
+data = pd.DataFrame(X, columns=dataset.feature_names)
+data = data.rename(columns={'od280/od315_of_diluted_wines': 'od_of_diluted_wines'})
+data = data.to_dict(orient='records')
 
-df = pd.read_parquet("s3://ml_data/testset.parquet")
-docs = df.to_dict(orient='records')
-
-class AngelTierPredictionUser(HttpUser):
+class WineModelPredictionUser(HttpUser):
     @task(1)
     def healthcheck(self):
         self.client.get("/healthcheck")
 
     @task(10)
     def prediction(self):
-        record = random.choice(docs).copy()
+        record = random.choice(data).copy()
         self.client.post("/predict", json=record)
 
     @task(2)
     def prediction_bad_value(self):
-        record = random.choice(docs).copy()
+        record = random.choice(data).copy()
         corrupt_key = random.choice(list(record.keys()))
         record[corrupt_key] = float("inf")
         self.client.post("/predict", json=record)
